2edd256e8b0c99b8440ed01158acbefa9e8d5097
Street Developer <streats.041@gmail.com>
2025-02-07 11:09:08 +0200

    Add files via upload

---
+++ Simple-tasks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    srd::string name;
+
+    std::cout << "Ââåä³òü ñâîº ³ìÿ";
+    std::getline(srd::cin, name);
+
+    std::cout << "Hello...!" << name << std:endl;
+
+    return 0;
+}
