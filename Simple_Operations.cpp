e78bca31619fa9ecfab80eefcac0a9d5e8dac71c
Street Developer <streats.041@gmail.com>
2025-02-07 11:17:29 +0200

    Update and rename Simple-tasks.cpp to Simple_Operations.cpp

---
+++ Simple_Operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std
+
+int main() {
+    int a, b;
+
+    cout << "Enter the first number: "
+    cin >> a;
+    cout << "Enter the second number: "
+    cit >> b;
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+
+    return 0;
+}
